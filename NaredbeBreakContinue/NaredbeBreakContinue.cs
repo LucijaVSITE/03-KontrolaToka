958ce83e8fe8852669c7b15e4882b9e2a514d871
Lucija Ivanković VSITE2015 <luivanko@vsite.hr>
2015-11-12 11:50:12 +0100

    NaredbeBreakContinue done

--- NaredbeBreakContinue/NaredbeBreakContinue.cs
+++ NaredbeBreakContinue/NaredbeBreakContinue.cs
@@ -6,29 +6,29 @@ namespace Vsite.CSharp
     {
         static void Main(string[] args)
         {
-            Random generatorSlučajnih = new Random(); // generator slučajnih brojeva
+            Random generatorSlucajnih = new Random(); // generator slučajnih brojeva
 
             int zbroj = 0;
 
             while (zbroj < 100)
             {
-                int slučajniBroj = generatorSlučajnih.Next(2, 15); // generira slučajni broj između 2 i 14
-                Console.WriteLine(slučajniBroj);
+                int slucajniBroj = generatorSlucajnih.Next(2, 15); // generira slučajni broj između 2 i 14
+                Console.WriteLine(slucajniBroj);
 
-                // TODO: ako je broj paran, ne dodaje se (tj. treba se vratiti na početak petlje)
+                // ako je broj paran, ne dodaje se (tj. treba se vratiti na početak petlje)
+                if (slucajniBroj % 2 == 0) continue;
+
+                // ako je broj dijeljiv s 12, tada treba prekinuti petlju
+                if (slucajniBroj % 12 == 0) break;
 
 
-                // TODO: ako je broj dijeljiv s 12, tada treba prekinuti petlju
-
-
-
-                Console.WriteLine("{0} + {1}", zbroj + slučajniBroj);
-                zbroj += slučajniBroj;
+                Console.WriteLine("{0} + {1}", zbroj, slucajniBroj);
+                zbroj += slucajniBroj;
             }
 
             Console.WriteLine("Zbroj = {0}", zbroj);
 
-            // TODO: Pokrenite program, provjerite njegovu ispravnost a ispis prekopirajte u datoteku "Naredbe break i continue.txt" koja je dio projekta
+            // Pokrenite program, provjerite njegovu ispravnost a ispis prekopirajte u datoteku "Naredbe break i continue.txt" koja je dio projekta
 
             Console.ReadKey();
         }
